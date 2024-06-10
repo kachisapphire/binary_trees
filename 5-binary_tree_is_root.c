cefda13516760f6cf5d9c8dba7dab681c7053dbc
kachisapphire <mychaelesther99@gmail.com>
2024-06-10 04:26:15 -0700

    task5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - function to exceute
+ * @node: a pointer to the node to check
+ * Return: 1 or 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
+}
