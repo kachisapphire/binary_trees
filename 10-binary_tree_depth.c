76471ae2507bd6f873c9a271fbdced5dd1f12b73
kachisapphire <mychaelesther99@gmail.com>
2024-06-14 08:50:05 -0700

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - function tp check fro depth
+ * @tree: pointer to the node to meausre the depth
+ * Return: 0
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return ((tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
